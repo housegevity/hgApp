ef3ed4ebc0d71ea4a9f364f3b4bfa0f6d44898e3
Joey Isaacson <joeisaacson23@gmail.com>
2014-01-26 20:09:31 -0500

    dashboard broken out into different views

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,6 @@
 
 /* Controllers */
 
-console.log('controllers loaded')
-
 angular.module('myApp.controllers', [])
 
 .controller('homeCtrl', function($scope, $location) {
@@ -11,64 +9,48 @@ angular.module('myApp.controllers', [])
 
 	$scope.signUp = function() {
 		$location.path('dash')
-		console.log('send me to the')
 	}
 
 	$scope.signIn = function() {
 		$scope.newUser = !$scope.newUser
-		console.log('WHAT THE FUCK')
 	}
 })
 
-.controller('dashCtrl', function($scope) {	
+.controller('dashCtrl', function($scope, myProperties, buyProperties) {	
 	console.log('home controller seperated')
 
+	$scope.myProperties = myProperties;
+	$scope.buyProperties = buyProperties;
+
+	$scope.myPropertieslength = myProperties.length;
+	$scope.buyPropertieslength = buyProperties.length;
+
 	$scope.addbuyProperty = function() {
-		$('#addProperty').modal('show')
-		console.log('show the addbuy modal')
+		$('#addProperty').modal('show');
 	}
 
-	$scope.myProperties = [
-		{
-			'name': 'Tiburon Mansion',
-			'city': 'Tiburon',
-			'state': 'CA',
-			'date_added': '1/2/12'
-		},
-		{
-			'name': 'Thugz Mansion',
-			'city': 'Tiburon',
-			'state': 'CA',
-			'date_added': '3/23/12'
-		},
-		{
-			'name': 'Ski House',
-			'city': 'Tahoe',
-			'state': 'CA',
-			'date_added': '2/29/13'
-		}
-	];
 
-	$scope.buyProperties = [
-		{
-			'name': '3 Bedroom Fixer Upper',
-			'city': 'Santa Rosa',
-			'state': 'CA',
-			'date_added': '2/3/14'
-		},
-		{
-			'name': '2 Bedroom Condo',
-			'city': 'Vallejo',
-			'state': 'CA',
-			'date_added': '4/10/20'
-		},
-		{
-			'name': 'Apartment',
-			'city': 'Salt Lake City',
-			'state': 'UT',
-			'date_added': '2/5/14'
-		}
-	];
+
+})
+
+.controller('myCtrl', function($scope, myProperties, buyProperties) {
+	console.log('myctrl loaded')
+
+	$scope.myProperties = myProperties;
+
+	$scope.addmyProperty = function() {
+		$('#addProperty').modal('show');
+	}
+})
+
+.controller('buyCtrl', function($scope, myProperties, buyProperties) {
+	console.log('buyctrl loaded')
+
+	$scope.buyProperties = buyProperties;
+
+	$scope.addbuyProperty = function() {
+		$('#addProperty').modal('show');
+	}
 })
 
 .controller('settingCtrl', function($scope, $location) {
--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,63 @@
 'use strict';
 
-/* Services */
+angular.module('myApp.services', [])
 
+.factory('myProperties', function() {
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+	var myProperties = [
+		{
+			'name': '3 Bedroom Fixer Upper',
+			'city': 'Santa Rosa',
+			'state': 'CA',
+			'date_added': '2/3/14'
+		},
+		{
+			'name': '2 Bedroom Condo',
+			'city': 'Vallejo',
+			'state': 'CA',
+			'date_added': '4/10/20'
+		},
+		{
+			'name': 'Sophias Apartment',
+			'city': 'Salt Lake City',
+			'state': 'UT',
+			'date_added': '2/5/14'
+		},		
+		{
+			'name': 'SICK HOUSE',
+			'city': 'Salt Lake City',
+			'state': 'UT',
+			'date_added': '2/5/14'
+		},
+	]
+
+	//return the array here
+	return myProperties
+})
+
+.factory('buyProperties', function() {
+
+	var buyProperties = [
+		{
+			'name': '3 Bedroom Fixer Upper',
+			'city': 'Santa Rosa',
+			'state': 'CA',
+			'date_added': '2/3/14'
+		},
+		{
+			'name': '2 Bedroom Condo',
+			'city': 'Vallejo',
+			'state': 'CA',
+			'date_added': '4/10/20'
+		},
+		{
+			'name': '2 Bedroom Condo',
+			'city': 'Vallejo',
+			'state': 'CA',
+			'date_added': '4/10/20'
+		},
+	];
+
+	//return the array here
+	return buyProperties
+});
