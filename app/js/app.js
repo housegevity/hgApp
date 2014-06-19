678731d02486f56dd2c2d1bc3223bea5f787621a
Tan Quach <tan.quach@crowdsurge.com>
2014-06-18 22:36:44 -0400

    Remove testData.js from index.html

--- app/js/app.js
+++ app/js/app.js
@@ -25,7 +25,6 @@ angular.module('hgApp', [
   'hgApp.service.firebase',
   'hgApp.service.propertyManager',
   'hgApp.service.documentManager',
-  'hgApp.service.testData', // Temporary for testing only
 
   // Directives
   'hgApp.directives.version',
