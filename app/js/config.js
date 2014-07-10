2e4ca40765a3a910222b9863bd4987770426d9ad
Tan Quach <tan.quach@gmail.com>
2014-07-09 22:12:50 -0400

    Fix gapi key

--- app/js/config.js
+++ app/js/config.js
@@ -13,10 +13,7 @@ angular.module('hgApp.config', [])
    // Google API Key for Cloud Storage
    .constant('GAPIKEY', 'AIzaSyBBGscNQ9LWYaO2tpAbq6Do5rJH58NKBY4')
 
-   // Local Google oAuth Key
-   // .constant('GAPI_CLIENTID', '815222281377-56mijm3a9t5scf69gd45p1kbro38uou3.apps.googleusercontent.com')
-
-   // Production Google oAuth Key
+   // Google oAuth Key
    .constant('GAPI_CLIENTID', '815222281377-7sb8cstkbakdrmhhskbhj33pbivcd5ch.apps.googleusercontent.com')
 
    // Google scopes
